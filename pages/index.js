c91e4818f8148e103f977d2858e488feb1fcfd2e
STARRY GEUST 2 <>
2022-09-01 17:48:43 +0900

    細部調整（全体フォント、作品タイトル幅調整、文字と画像ワンセット（あともう少し））

--- pages/index.js
+++ pages/index.js
@@ -6,70 +6,43 @@ import styles from '../styles/Home.module.css'
 import top from '../styles/top.module.css'
 import name from '../styles/name.module.css'
 import production from '../styles/production.module.css'
-import theWork from '../styles/theWork.module.css'
+import work from '../styles/work.module.css'
 import qualification from '../styles/qualification.module.css'
 
 
 export default function Home() {
     return (
       <>
-        <div className="top">
-          <div className="row">
-            <h1 className={top.title}>履歴書</h1>
-          </div>
-        </div>
-        
-          <div className="name">
-          <div className="flex">
-            <div className="col-2 debug"> 
+        <Head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link 
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
+        </Head>
+
+          <h1 className={top.title}>履歴書</h1>
+          <name className={name.beside}>
               <img className="imag-responsive center-block" src={"/image/face.png"} width={"30%"}/>
-            </div>
-            <div className="col-5 debug">
               <p className={name.myName}>民谷友菜/19歳/女</p>
-            </div>
-          </div>
-        </div>
+          </name>
 
-        <div className="production">
-          <div className="row">
-            <div className={production.parent}>
-              <span className="item">
-                <h2 title={production.title}>fusion360学生デザインコンテスト応募作品集</h2>
-              </span>
-            </div>
-          </div>
-        </div>
+          <production className={production.parent}>
+              <div className={production.wrapper}><h2 className={production.title}>fusion360学生デザインコンテスト応募作品集</h2></div>
+          </production>
 
-        <div className="theWork">
-          <div className="flex">
-            <div className="col-5 debug">
-              <h3 className={theWork.juice}>「ラムネ瓶」</h3>
-              <img className="imag-responsive center-block" src={"/image/juice.png"} width={"30%"}/>
-            </div>
-            <div className="col-2 debug"> </div>
-            <div className="col-5 debug">
-              <p className>  </p>
-            </div>
-            <div className="col-5 debug">
-              <p className>  </p>
-            </div>
-            <div className="col-2 debug"></div>
-            <div className="col-5 debug">
-              <h4 className={theWork.soba}>「ざるそば」</h4>
-              <img className="imag-responsive center-block" src={"/image/soba.png"} width={"30%"}/>
-              <p className>  </p>
-            </div>
-          </div>
-        </div>
+          <work className={work.beside}>
+            <work className={work.group}>
+              <h3 className={work.juice}>「ラムネ瓶」</h3>
+              <img className="imag-responsive center-block" src={"/image/juice.png"} width={"50%"}/>
+            <work className={work.group}></work>
+            <work className={work.group}>
+              <h4 className={work.soba}>「ざるそば」</h4>
+              <img className="imag-responsive center-block" src={"/image/soba.png"} width={"50%"}/>
+            </work>
+          </work>
 
-        <div className="qualification">
-          <div className="row">
-            <div className="col-12 debug">
-            <h5 className={qualification.qualification}>資格・免許</h5>
-            <p className={qualification.name}>・自動車運転免許</p>
-            </div>
-          </div>
-        </div>
+          <h5 className={qualification.qualification}>資格・免許</h5>
+          <p className={qualification.name}>・自動車運転免許（仮）</p>
       </>
     );  
 }
