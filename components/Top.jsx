55fd6802c76d87784a2289e643f3cc053b467994
STARRY GEUST 2 <>
2022-09-06 12:22:55 +0900

    scss 列合わせ

--- components/Top.jsx
+++ components/Top.jsx
@@ -10,6 +10,7 @@ export default function Top() {
             <meta charset="UTF-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             </Head>
+            
             <h1 className={top.title}>履歴書</h1>
             <div className={top.beside}>
               <img className={top.img_F} src={"/image/face.png"}/>
