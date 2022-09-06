fba9b924f887a8bae294483e328a94eca21618c6
STARRY GEUST 2 <>
2022-09-06 17:56:21 +0900

    Top.jsx 復元

---
+++ components/Top.jsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import React from 'react'
+import top from '../styles/top.module.scss'
+
+
+export default function Top() {
+    return(
+        <>
+            <Head>
+            <meta charset="UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <link 
+                href='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/gsap.min.js'
+                rel='stylesheet'
+            />
+            </Head>
+
+            <h1 className={top.title}>履歴書</h1>
+            <div className={top.beside}>
+              <img className={top.face_img} src={"/image/face.png"}/>
+              <p className={top.myName}>民谷友菜/19歳/女</p>
+            </div>
+        </>
+    );
+}
