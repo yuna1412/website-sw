b04b221ba659df4af384022c2428bd08627a70fd
STARRY GEUST 2 <>
2022-09-08 18:00:32 +0900

    モーダルにgsapアニメーション付与

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -7,26 +7,41 @@ import modal from '../styles/modal.module.scss'
 
 export default function Modal({show, setShow}) {
   const text = useRef();
+  // let flg = false
+
+  //  const window = () => {
+  //    if(flg == false){
+  //        gsap.to(modalText.current, {
+  //            opacity: 0
+  //            });
+  //        flg=true
+  //    }else{
+  //        gsap.to(modalText.current, {
+  //            opacity: 1
+  //            });
+  //        flg=false
+  //    }
+  //  }  
+
   const closeModel = () => {
-    
     gsap.to(text.current, {
       opacity: 0,
       onComplete: ()=>{
-        setShow(false)
+        // setShow(false)
       }
     });
   }
 
-  if(show) {
+  // if(show) {
     return (
-      <div className={modal.overlay}>
-        <div className={modal.content}>
-          <p ref={text}>モーダルウィンドウ</p>
-          <button onClick={closeModel}>close</button>
+        <div className={modal.overlay}>
+          <div className={modal.content}>
+            <p ref={text}>モーダルウィンドウ</p>
+            <button onClick={closeModel}>close</button>
+          </div>
         </div>
-      </div>
     )
-  } else {
-    return null;
-  }
+  // } else {
+  //   return null;
+  // }
 }
--- pages/modal.js
+++ pages/modal.js
@@ -1,14 +1,36 @@
 import Head from 'next/head'
+import { gsap }  from 'gsap';
 import Modal from '../components/Modal';
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 
 export default function modal() {
   const [show, setShow] = useState(false)
-  return (
+  const modal= useRef();
+  let isFirst = false
+
+  
+
+  useEffect(() => {
+    if(isFirst) return
+    isFirst = true
+    window.addEventListener("mousedown", ()=>{
+      gsap.set(modal.current, {
+        opacity: 0,
+      })        
+      gsap.to(modal.current, {
+        opacity: 1,
+        duration:2.5
+      })
+    })
+  });
+
+  return(
     <div>
-      <button onClick={() => setShow(true)}>Click</button>
-      <Modal show={show} setShow={setShow}/>
+    {/* <button onClick={() => setShow(true)}>Click</button> */}
+      <div ref={modal}>
+        <Modal/>
+      </div>
     </div>
   );  
 }
